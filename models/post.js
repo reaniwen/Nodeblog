bae4fbaa05b29b64ab4d82b60b76f5fdb5fe94ae
reaniwen <reaniwen@gmail.com>
2016-01-29 23:30:02 -0500

    comment achieved

--- models/post.js
+++ models/post.js
@@ -25,7 +25,8 @@ Post.prototype.save = function(callback) {
 		name: this.name,
 		time: time,
 		title: this.title,
-		post: this.post
+		post: this.post,
+		comments: []
 	};
 	// open db
 	mongodb.open(function (err, db) {
@@ -110,7 +111,13 @@ Post.getOne = function(name, day, title, callback) {
 					return callback(err);
 				}
 
-				doc.post = markdown.toHTML(doc.post);
+				// doc.post = markdown.toHTML(doc.post);
+				if (doc) {
+					doc.post = markdown.toHTML(doc.post);
+					doc.comments.forEach(function (comment) {
+						comment.content = markdown.toHTML(comment.content);
+					});
+				}
 				callback(null, doc);
 			});
 		});
@@ -140,6 +147,35 @@ Post.edit = function(name, day, title, callback) {
 					return callback(err);
 				}
 				callback(null, doc);
+			});
+		});
+	});
+};
+
+// Update an article
+Post.update = function(name, day, title, post, callback) {
+	mongodb.open(function (err, db) {
+		if (err) {
+			return callback(err);
+		}
+		db.collection('posts', function (err, collection) {
+		if (err) {
+			mongodb.close();
+			return callback(err);
+		}
+		// update article
+		collection.update({
+			"name": name,
+			"time.day": day,
+			"title": title
+		}, {
+			$set: {post: post}
+		}, function (err) {
+			mongodb.close();
+			if (err) {
+				return callback(err);
+			}
+			callback(null);
 			});
 		});
 	});
